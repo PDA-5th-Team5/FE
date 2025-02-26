2e9d9f5ecb0777a7ea66b88ce6b92c43a7f4d8ca
ZZZINU <nnjin987@gmail.com>
2025-02-26 10:37:31 +0900

    feat(#3): 메인 페이지 - 추천 필터 UI 구현

--- src/pages/mainPage/MainPage.styled.ts
+++ src/pages/mainPage/MainPage.styled.ts
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 export const MainPageContainer = styled.div`
   display: flex;
+  flex-direction: column;
   margin: 40px 0px;
+  gap: 24px;
 `;
 
 export const MainPageHeader = styled.div`
@@ -41,4 +43,87 @@ export const MainPageHeaderButton = styled.div`
 
   border-radius: 8px;
   background: #2595e0;
+`;
+
+// 추천 필터
+export const MainPageBox = styled.div`
+  border-radius: 8px;
+  background: #1b212d;
+  padding: 20px;
+`;
+
+export const MainPageTabWrapper = styled.div`
+  display: flex;
+`;
+
+export const MainPageTab = styled.div<{ isActive: boolean }>`
+  color: ${({ isActive }) => (isActive ? "#fff" : "#8B8D91")};
+  font-size: 16px;
+  font-weight: ${({ isActive }) => (isActive ? "700" : "400")};
+  border-bottom: 2px solid
+    ${({ isActive }) => (isActive ? "#fff" : "transparent")};
+  padding: 11px 22px;
+  cursor: pointer;
+
+  &:hover {
+    color: #fff;
+  }
+`;
+
+export const MainPageRecommendedFilterList = styled.div`
+  display: flex;
+  margin-top: 16px;
+  gap: 20px;
+
+  /* 가로스크롤 */
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  overflow-y: hidden;
+
+  /* 스크롤바 숨기기 */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+`;
+
+export const MainPageRecommendedFilterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 306px;
+  height: 64px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  border: 1px solid #2c333d;
+  background: #202833;
+  gap: 16px;
+  padding: 14px 20px;
+  box-sizing: border-box;
+`;
+
+export const MainPageRecommendedFilterImg = styled.img`
+  height: 36px;
+`;
+
+export const MainPageRecommendedFilterInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+export const MainPageRecommendedFilterTitle = styled.div`
+  color: #fff;
+  font-size: 14px;
+`;
+
+export const MainPageRecommendedFilterCntWrapper = styled.div`
+  display: flex;
+  gap: 2px;
+`;
+export const MainPageRecommendedFilterCntImg = styled.img`
+  width: 13px;
+`;
+export const MainPageRecommendedFilterCnt = styled.div`
+  color: #dbdcdd;
+  font-size: 12px;
 `;
--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import * as S from "./MainPage.styled";
+import ExampleImg from "../../assets/images/common/example.png";
+import ImportIcon from "../../assets/images/common/icons/import.png";
 
 const MainPage: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<"popular" | "investor">("popular");
   return (
     <S.MainPageContainer>
       <S.MainPageHeader>
@@ -10,6 +14,107 @@ const MainPage: React.FC = () => {
           <S.MainPageHeaderButton>저장</S.MainPageHeaderButton>
         </S.MainPageHeaderButtonWrapper>
       </S.MainPageHeader>
+
+      <S.MainPageBox>
+        <S.MainPageTabWrapper>
+          <S.MainPageTab
+            isActive={activeTab === "popular"}
+            onClick={() => setActiveTab("popular")}
+          >
+            인기있는 필터
+          </S.MainPageTab>
+          <S.MainPageTab
+            isActive={activeTab === "investor"}
+            onClick={() => setActiveTab("investor")}
+          >
+            유명 투자자 필터
+          </S.MainPageTab>
+        </S.MainPageTabWrapper>
+
+        <S.MainPageRecommendedFilterList>
+          {/* ----- 더미데이터 ----- */}
+          <S.MainPageRecommendedFilterWrapper>
+            <S.MainPageRecommendedFilterImg src={ExampleImg} />
+            <S.MainPageRecommendedFilterInfo>
+              <S.MainPageRecommendedFilterTitle>
+                인기있는 필터링 Top10
+              </S.MainPageRecommendedFilterTitle>
+
+              <S.MainPageRecommendedFilterCntWrapper>
+                <S.MainPageRecommendedFilterCntImg src={ImportIcon} />
+                <S.MainPageRecommendedFilterCnt>
+                  12,000
+                </S.MainPageRecommendedFilterCnt>
+              </S.MainPageRecommendedFilterCntWrapper>
+            </S.MainPageRecommendedFilterInfo>
+          </S.MainPageRecommendedFilterWrapper>
+
+          <S.MainPageRecommendedFilterWrapper>
+            <S.MainPageRecommendedFilterImg src={ExampleImg} />
+            <S.MainPageRecommendedFilterInfo>
+              <S.MainPageRecommendedFilterTitle>
+                인기있는 필터링 Top10
+              </S.MainPageRecommendedFilterTitle>
+
+              <S.MainPageRecommendedFilterCntWrapper>
+                <S.MainPageRecommendedFilterCntImg src={ImportIcon} />
+                <S.MainPageRecommendedFilterCnt>
+                  12,000
+                </S.MainPageRecommendedFilterCnt>
+              </S.MainPageRecommendedFilterCntWrapper>
+            </S.MainPageRecommendedFilterInfo>
+          </S.MainPageRecommendedFilterWrapper>
+
+          <S.MainPageRecommendedFilterWrapper>
+            <S.MainPageRecommendedFilterImg src={ExampleImg} />
+            <S.MainPageRecommendedFilterInfo>
+              <S.MainPageRecommendedFilterTitle>
+                인기있는 필터링 Top10
+              </S.MainPageRecommendedFilterTitle>
+
+              <S.MainPageRecommendedFilterCntWrapper>
+                <S.MainPageRecommendedFilterCntImg src={ImportIcon} />
+                <S.MainPageRecommendedFilterCnt>
+                  12,000
+                </S.MainPageRecommendedFilterCnt>
+              </S.MainPageRecommendedFilterCntWrapper>
+            </S.MainPageRecommendedFilterInfo>
+          </S.MainPageRecommendedFilterWrapper>
+
+          <S.MainPageRecommendedFilterWrapper>
+            <S.MainPageRecommendedFilterImg src={ExampleImg} />
+            <S.MainPageRecommendedFilterInfo>
+              <S.MainPageRecommendedFilterTitle>
+                인기있는 필터링 Top10
+              </S.MainPageRecommendedFilterTitle>
+
+              <S.MainPageRecommendedFilterCntWrapper>
+                <S.MainPageRecommendedFilterCntImg src={ImportIcon} />
+                <S.MainPageRecommendedFilterCnt>
+                  12,000
+                </S.MainPageRecommendedFilterCnt>
+              </S.MainPageRecommendedFilterCntWrapper>
+            </S.MainPageRecommendedFilterInfo>
+          </S.MainPageRecommendedFilterWrapper>
+
+          <S.MainPageRecommendedFilterWrapper>
+            <S.MainPageRecommendedFilterImg src={ExampleImg} />
+            <S.MainPageRecommendedFilterInfo>
+              <S.MainPageRecommendedFilterTitle>
+                인기있는 필터링 Top10
+              </S.MainPageRecommendedFilterTitle>
+
+              <S.MainPageRecommendedFilterCntWrapper>
+                <S.MainPageRecommendedFilterCntImg src={ImportIcon} />
+                <S.MainPageRecommendedFilterCnt>
+                  12,000
+                </S.MainPageRecommendedFilterCnt>
+              </S.MainPageRecommendedFilterCntWrapper>
+            </S.MainPageRecommendedFilterInfo>
+          </S.MainPageRecommendedFilterWrapper>
+          {/* ----- 더미데이터 ----- */}
+        </S.MainPageRecommendedFilterList>
+      </S.MainPageBox>
     </S.MainPageContainer>
   );
 };
