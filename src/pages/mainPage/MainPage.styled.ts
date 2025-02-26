0628b8cf12a332f81d3c721385a805c4872df390
ZZZINU <nnjin987@gmail.com>
2025-02-26 09:34:47 +0900

    design(#3): 메인 페이지 - 헤더 생성

--- src/pages/mainPage/MainPage.styled.ts
+++ src/pages/mainPage/MainPage.styled.ts
@@ -2,5 +2,43 @@ import styled from "styled-components";
 
 export const MainPageContainer = styled.div`
   display: flex;
-  background-color: pink;
+  margin: 40px 0px;
+`;
+
+export const MainPageHeader = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const MainPageTitle = styled.div`
+  color: #fff;
+  font-size: 28px;
+  font-weight: 700;
+`;
+
+export const MainPageHeaderButtonWrapper = styled.div`
+  display: flex;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 400;
+  gap: 24px;
+`;
+
+export const MainPageHeaderReset = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MainPageHeaderButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 64px;
+  height: 40px;
+
+  border-radius: 8px;
+  background: #2595e0;
 `;
--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,5 +1,17 @@
 import * as S from "./MainPage.styled";
 
-export default function MainPage() {
-  return <S.MainPageContainer>MainPage</S.MainPageContainer>;
-}
+const MainPage: React.FC = () => {
+  return (
+    <S.MainPageContainer>
+      <S.MainPageHeader>
+        <S.MainPageTitle>Stock Snowper</S.MainPageTitle>
+        <S.MainPageHeaderButtonWrapper>
+          <S.MainPageHeaderReset>초기화</S.MainPageHeaderReset>
+          <S.MainPageHeaderButton>저장</S.MainPageHeaderButton>
+        </S.MainPageHeaderButtonWrapper>
+      </S.MainPageHeader>
+    </S.MainPageContainer>
+  );
+};
+
+export default MainPage;
