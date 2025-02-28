640c8685bc28c628b78241e81471586bd38ccfc8
ZZZINU <nnjin987@gmail.com>
2025-02-28 13:33:37 +0900

    design(#7): 공유 포트폴리오 목록 - 타이틀 및 정렬 UI 구현

---
+++ src/pages/sharePortfolioPage/SharePortfolioPage.styled.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const SharePortfolioListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 64px 0px;
+`;
+
+export const SharePortfolioListTitle = styled.div`
+  color: #fff;
+  font-size: 28px;
+  font-weight: 700;
+`;
+
+export const SharePortfolioListSubTitle = styled.div`
+  color: #fff;
+  font-size: 16px;
+  margin-top: 20px;
+`;
+
+export const SharePortfolioListHeader = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 80px;
+`;
+
+export const SharePortfolioListCount = styled.div`
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+export const SharePortfolioList = styled.div`
+  display: grid;
+`;
--- src/pages/sharePortfolioPage/SharePortfolioPage.tsx
+++ src/pages/sharePortfolioPage/SharePortfolioPage.tsx
@@ -1,3 +1,34 @@
-export default function SharePortfolioPage() {
-  return <>SharePortfolioPage</>;
-}
+import { useState } from "react";
+import SortDropdown from "../../components/sortDropdown/SortDropdown";
+import * as S from "./SharePortfolioPage.styled";
+import DropdownIcon from "../../assets/images/icons/arrowDonw_gray.png";
+
+const SharePortfolioPage = () => {
+  const [sortKey, setSortKey] = useState("최신순");
+
+  return (
+    <S.SharePortfolioListContainer>
+      <S.SharePortfolioListTitle>
+        공유 포트폴리오 모아보기
+      </S.SharePortfolioListTitle>
+      <S.SharePortfolioListSubTitle>
+        다른 사람들의 포트폴리오를 보고, 그들의 전략을 참고하여 더 나은 투자
+        결정을 내려보세요
+      </S.SharePortfolioListSubTitle>
+
+      <S.SharePortfolioListHeader>
+        <S.SharePortfolioListCount>총 1,234개</S.SharePortfolioListCount>
+        <SortDropdown
+          options={["최신순", "인기순"]}
+          selected={sortKey}
+          onChange={setSortKey}
+          icon={DropdownIcon}
+        />
+      </S.SharePortfolioListHeader>
+
+      <S.SharePortfolioList></S.SharePortfolioList>
+    </S.SharePortfolioListContainer>
+  );
+};
+
+export default SharePortfolioPage;
