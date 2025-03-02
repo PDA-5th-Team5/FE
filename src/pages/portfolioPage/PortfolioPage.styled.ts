d229f0a4b7e5195293fd9deb1dfe73e61274acdd
ZZZINU <nnjin987@gmail.com>
2025-03-02 12:28:23 +0900

    design(#9): 나의 포트폴리오 페이지 - 포트폴리오 요약 box ui 구현

--- src/pages/portfolioPage/PortfolioPage.styled.ts
+++ src/pages/portfolioPage/PortfolioPage.styled.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const PortfolioPageContainer = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
   margin-top: 30px;
 `;
@@ -10,4 +11,92 @@ export const PortfolioDescription = styled.div`
   color: #fff;
   font-size: 16px;
   line-height: 30px;
+`;
+
+export const PortfolioContent = styled.div`
+  display: flex;
+  gap: 16px;
+  margin-top: 80px;
+`;
+
+export const PortfolioContentLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+// 포트폴리오 요약
+export const PortfolioSummary = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 767px;
+  height: 116px;
+  border-radius: 8px;
+  background: #1c242e;
+  box-sizing: border-box;
+  padding: 20px 40px;
+`;
+
+export const PortfolioSummaryItemWrapper = styled.div`
+  display: flex;
+`;
+
+export const PortfolioSummaryItem = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: flex-start;
+  gap: 16px;
+  width: 148px;
+  margin-right: 20px;
+  box-sizing: border-box;
+`;
+
+export const PortfolioSummaryItemLine = styled.div<{ $isFirst?: boolean }>`
+  width: 4px;
+  height: 49px;
+  background-color: ${({ $isFirst }) => ($isFirst ? "#2394df" : "#3E4855")};
+  border-radius: 8px;
+`;
+
+export const PortfolioSummaryItemData = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 4px;
+  gap: 4px;
+`;
+export const PortfolioSummaryItemDataValue = styled.div`
+  color: #fff;
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export const PortfolioSummaryItemDataTitle = styled.div`
+  color: #a7abae;
+  font-size: 12px;
+`;
+
+export const PortfolioContentTitle = styled.div`
+  color: #fff;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+export const PortfolioLineGraph = styled.div`
+  width: 767px;
+  height: 348px;
+  border-radius: 8px;
+  background: #1a222d;
+  box-sizing: border-box;
+  padding: 20px 40px;
+`;
+
+export const PortfolioContentRight = styled.div`
+  width: 417px;
+  height: 480px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: #1c242e;
+  box-sizing: border-box;
+  padding: 20px 40px;
 `;
--- src/pages/portfolioPage/PortfolioPage.tsx
+++ src/pages/portfolioPage/PortfolioPage.tsx
@@ -11,6 +11,74 @@ const PortfolioPage = () => {
         제가 쓴 포트폴리오 123에 대한 설명을 적을겁니다 제가 쓴 포트폴리오 123에
         대한 설명을 적을겁니다 제가 쓴 포트폴리오 123에 대한 설명을 적을겁니다
       </S.PortfolioDescription>
+
+      <S.PortfolioContent>
+        <S.PortfolioContentLeft>
+          <S.PortfolioSummary>
+            <S.PortfolioContentTitle>포트폴리오 요약</S.PortfolioContentTitle>
+            <S.PortfolioSummaryItemWrapper>
+              {/* 이렇게가 하나입니다! */}
+              <S.PortfolioSummaryItem>
+                <S.PortfolioSummaryItemLine $isFirst={true} />
+                <S.PortfolioSummaryItemData>
+                  <S.PortfolioSummaryItemDataValue>
+                    50조
+                  </S.PortfolioSummaryItemDataValue>
+                  <S.PortfolioSummaryItemDataTitle>
+                    평균 시가총액
+                  </S.PortfolioSummaryItemDataTitle>
+                </S.PortfolioSummaryItemData>
+              </S.PortfolioSummaryItem>
+
+              {/* 이렇게가 하나입니다! */}
+              <S.PortfolioSummaryItem>
+                <S.PortfolioSummaryItemLine />
+                <S.PortfolioSummaryItemData>
+                  <S.PortfolioSummaryItemDataValue>
+                    23.08
+                  </S.PortfolioSummaryItemDataValue>
+                  <S.PortfolioSummaryItemDataTitle>
+                    평균 PER
+                  </S.PortfolioSummaryItemDataTitle>
+                </S.PortfolioSummaryItemData>
+              </S.PortfolioSummaryItem>
+
+              {/* 이렇게가 하나입니다! */}
+              <S.PortfolioSummaryItem>
+                <S.PortfolioSummaryItemLine />
+                <S.PortfolioSummaryItemData>
+                  <S.PortfolioSummaryItemDataValue>
+                    25.01%
+                  </S.PortfolioSummaryItemDataValue>
+                  <S.PortfolioSummaryItemDataTitle>
+                    평균 부채비율
+                  </S.PortfolioSummaryItemDataTitle>
+                </S.PortfolioSummaryItemData>
+              </S.PortfolioSummaryItem>
+
+              {/* 이렇게가 하나입니다! */}
+              <S.PortfolioSummaryItem>
+                <S.PortfolioSummaryItemLine />
+                <S.PortfolioSummaryItemData>
+                  <S.PortfolioSummaryItemDataValue>
+                    2.08
+                  </S.PortfolioSummaryItemDataValue>
+                  <S.PortfolioSummaryItemDataTitle>
+                    평균 배당
+                  </S.PortfolioSummaryItemDataTitle>
+                </S.PortfolioSummaryItemData>
+              </S.PortfolioSummaryItem>
+            </S.PortfolioSummaryItemWrapper>
+          </S.PortfolioSummary>
+          <S.PortfolioLineGraph></S.PortfolioLineGraph>
+        </S.PortfolioContentLeft>
+
+        <S.PortfolioContentRight>
+          <S.PortfolioContentTitle>
+            포트폴리오 Snowflake
+          </S.PortfolioContentTitle>
+        </S.PortfolioContentRight>
+      </S.PortfolioContent>
     </S.PortfolioPageContainer>
   );
 };
