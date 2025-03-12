a3de32d6a88375f3dd0219a988a0e2505561eea5
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-03-12 11:14:59 +0900

    feat(38): 나의 포트폴리오 리스트 조회

--- src/apis/portfolio.ts
+++ src/apis/portfolio.ts
@@ -1,5 +1,5 @@
 import { APIResponse, portfolioAPI } from ".";
-import { PortfolioDetail } from "../types/portfolioTypes";
+import { PortfolioDetail,MyPortfolioResponse } from "../types/portfolioTypes";
 import { SnowflakeItems } from "../types/snowflakeTypes";
 
 // 1) 인기 포트폴리오 조회 & 전문가 포트폴리오 조회
@@ -42,3 +42,76 @@ export const saveMyPortfolioAPI = async (payload: SaveMyPortfolio) => {
   });
   return response.data;
 };
+<<<<<<< Updated upstream
+=======
+
+//공유포폴리스트조회
+export interface RangeValue {
+  min: number;
+  max: number;
+}
+export interface SharePortfolioItem {
+  sharePortfolioId: number;
+  loadCount: number;
+  createdAt: string;
+  portfolio: {
+    id: string;
+    title: string;
+    description?: string;
+    category: string;
+    portfolioId: number;
+    market?: string;
+    sector?: string[];
+    
+    marketCap?: RangeValue;
+    per?: RangeValue;
+    eps?: RangeValue;
+    bps?: RangeValue;
+    pbr?: RangeValue;
+    dividendYield?: RangeValue;
+    foreignerRatio?: RangeValue;
+    sps?: RangeValue;
+    saleAccount?: RangeValue;
+    crntRate?: RangeValue;
+    lbltRate?: RangeValue;
+    ntinInrt?: RangeValue;
+    bsopPrfiInrt?: RangeValue;
+    grs?: RangeValue;
+    roeVal?: RangeValue;
+    bsopPrti?: RangeValue;
+    thtrNtin?: RangeValue;
+  };
+}
+
+export interface APIResponse<T> {
+  status: number;
+  message: string;
+  data: T;
+}
+
+export const sharePortfolioListAPI = async(sortBy: string = "loadCount"): Promise<SharePortfolioItem[]> => {
+  const response = await portfolioAPI.get<APIResponse<SharePortfolioItem[]>>(
+    `/share/board?page=0&sortBy=${sortBy}`
+  );
+  return response.data.data;
+}
+
+// 나의 포트폴리오 리스트 조회 API
+export const myPortfolioListAPI = async (): Promise<MyPortfolioResponse[]> => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (!accessToken) {
+    throw new Error("로그인 해주세요!");
+  }
+
+  const response = await portfolioAPI.get<APIResponse<MyPortfolioResponse[]>>(
+    "/my",
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  return response.data.data;
+};
+>>>>>>> Stashed changes
