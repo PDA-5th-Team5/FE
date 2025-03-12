a8e0d88fd47ae4659b737c1dc2aaf5bf79d90d34
ehdms1115@naver.com <ehdms1115@naver.com>
2025-03-12 17:23:26 +0900

    feat(#88): 공유포트폴리오 댓글 작성/조회

--- src/apis/portfolio.ts
+++ src/apis/portfolio.ts
@@ -158,3 +158,107 @@ export const getMyPortfolioDetailAPI = async(portfolioId: number): Promise<Portf
   );
   return response.data.data;
 }
+
+
+//댓글
+//댓글 작성
+export interface CommentRequest {
+  content: string;
+}
+
+export const createPortfolioCommentAPI = async (
+  sharePortfolioId: number,
+  content: string
+): Promise<APIResponse<null>> => {
+  const response = await portfolioAPI.post<APIResponse<null>>(
+    `/share/${sharePortfolioId}/comments`,
+    JSON.stringify({ content }),
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        'Content-Type': 'application/json' 
+      },
+      transformRequest: [(data) => {
+        return data;
+      }]
+    }
+  );
+  
+  return response.data;
+};
+
+// 댓글 조회 API
+export interface Comment {
+  commentId: number;
+  nickname: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  stockId: number;
+  userId: string;
+}
+
+export interface CommentsResponse {
+  commentCnt: number;
+  comments: Comment[];
+}
+
+export const getPortfolioCommentsAPI = async (
+  sharePortfolioId: number,
+
+): Promise<CommentsResponse> => {
+  const response = await portfolioAPI.get<APIResponse<CommentsResponse>>(
+    `/share/${sharePortfolioId}/comments`
+  );
+  console.log(response)
+  // API 응답 형식에 맞게 데이터 변환
+  return {
+    commentCnt: response.data.data.commentCnt,
+    comments: response.data.data.comments
+  };
+};
+
+
+// 댓글 삭제 API
+export const deleteCommentAPI = async (
+  stockId: number,
+  commentId: number
+): Promise<APIResponse<null>> => {
+  const response = await portfolioAPI.delete<APIResponse<null>>(
+    `/${stockId}/comments/${commentId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+      }
+    }
+  );
+  
+  return response.data;
+};
+
+//댓글 수정
+export interface CommentRequest {
+  content: string;
+}
+
+export const editCommentAPI = async (
+  stockId: number,
+  commentId : number,
+  content: string
+): Promise<APIResponse<null>> => {
+  const response = await portfolioAPI.patch<APIResponse<null>>(
+    `/${stockId}/comments/${commentId}`,
+    content,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        'Content-Type': 'text/plain'
+      },
+      transformRequest: [(data) => {
+        return data;
+      }]
+    }
+  );
+  
+  return response.data;
+};
